0c019b90b1cf8290f1443e3ee9654275a0a3a32c
Vladimir Stamenov <vukannewcastle@gmail.com>
2023-02-08 13:24:14 +0200

    Just added folder

---
+++ Black Metal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Add several numbers, separated by a comma, e.g '1,2,3,4'");
+        string inputLine = Console.ReadLine();
+
+        // We can use string.Split('separator') 
+        // It will produce an array of strings:
+        string[] numbersAsString = inputLine.Split(',');
+
+        int[] powers = new int[numbersAsString.Length];
+        int index = 0;
+        foreach (var numAsString in numbersAsString)
+        {
+            int number = int.Parse(numAsString);
+            powers[index] = number * number;
+            index++;
+        }
+
+        // We can now output the powers, joined by a separator
+        // use the string.Join() method:
+        string output = string.Join(", ", powers);
+        Console.WriteLine($"Powers: {output}");
+    }
+}
